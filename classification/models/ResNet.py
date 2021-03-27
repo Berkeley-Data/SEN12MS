dbf78e60b2ce7164997408b83663a879655308b1
suryag@berkeley.edu <suryag@berkeley.edu>
2021-03-27 12:24:23 -0700

    CHanged the ResNet50_1x1 kernel size to 7,7 from 256,256

--- classification/models/ResNet.py
+++ classification/models/ResNet.py
@@ -135,7 +135,8 @@ class ResNet50_1x1(nn.Module):
             nn.ReLU(inplace=True)
         )
 
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=(256, 256), stride=(2, 2), padding=(3, 3), bias=False)
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+        # self.conv1 = nn.Conv2d(3, 64, kernel_size=(256, 256), stride=(2, 2), padding=(3, 3), bias=False)
         # self.conv1 = nn.Conv2d(n_inputs, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
         self.encoder = nn.Sequential(
             self.Conv1x1Block,
