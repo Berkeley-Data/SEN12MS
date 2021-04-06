3184f88781a8cc012c0f2b6ffc8661a27a275d74
taeil <taeil.goh@gmail.com>
2021-04-05 21:13:13 -0700

    updated parameters

--- classification/run_finetune.sh
+++ classification/run_finetune.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
-for lr in 0.001 0.0005 0.0001 0.00005 0.00001
+for dataset in bigearthnet sen12ms
 do
-  for epoch in 200
+  for lr in 0.001 0.0005 0.0001 0.00005 0.00001
   do
-    for label_tp in single_label multi_label
+    for epoch in 200
     do
-      for model in Moco # Moco_1x1RND Moco_1x1
+      for label_tp in single_label multi_label
       do
-        python classification/main_train.py --exp_name finetune --simple_scheme \
-                                            --lr ${lr} --use_lr_step --lr_step_size 30 --decay 1e-5 \
-                                            --pt_name electric-mountain-33 --pt_dir pretrained/moco \
-                                            --batch_size 64 --num_workers 4 --data_size 1024 \
-                                            --dataset sen12ms --label_split_dir splits \
-                                            --label_type ${label_tp} \
-                                            --model ${model} \
-                                            --epochs ${epoch} \
-                                            --sensor_type s1s2 --eval
+        for model in Moco # Moco_1x1RND Moco_1x1
+        do
+          python classification/main_train.py --exp_name finetune --simple_scheme \
+                                              --lr ${lr} --use_lr_step --lr_step_size 30 --decay 1e-5 \
+                                              --pt_name electric-mountain-33 --pt_dir pretrained/moco \
+                                              --batch_size 64 --num_workers 4 --data_size 1024 \
+                                              --dataset ${dataset} --label_split_dir splits \
+                                              --label_type ${label_tp} \
+                                              --model ${model} \
+                                              --epochs ${epoch} \
+                                              --sensor_type s1s2 --eval
+        done
       done
     done
   done
 done
-#
--- classification/run_supervised.sh
+++ classification/run_supervised.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-for lr in 0.00001 0.00005 0.0001 0.001 0.001
+for dataset in bigearthnet # sen12ms
 do
-  for epoch in 200
+  for lr in 0.001 0.0005 0.0001 0.00005 0.00001
   do
-    for label_tp in single_label multi_label
+    for epoch in 200
     do
-      for model in Supervised_1x1 Supervised
+      for label_tp in single_label multi_label
       do
-        python classification/main_train.py --exp_name sup_learning --simple_scheme \
-                                            --lr ${lr} --use_lr_step --lr_step_size 30 --decay 1e-5 \
-                                            --batch_size 64 --num_workers 4 --data_size 1024 \
-                                            --dataset sen12ms --label_split_dir splits \
-                                            --label_type ${label_tp} \
-                                            --model ${model} \
-                                            --epochs ${epoch} \
-                                            --sensor_type s1s2 --eval
+        for model in Supervised # Supervised_1x1
+        do
+          python classification/main_train2.py --exp_name sup_learning --simple_scheme \
+                                              --lr ${lr} --use_lr_step --lr_step_size 30 --decay 1e-5 \
+                                              --batch_size 64 --num_workers 4 --data_size 1024 \
+                                              --dataset ${dataset} --label_split_dir splits \
+                                              --label_type ${label_tp} \
+                                              --model ${model} \
+                                              --epochs ${epoch} \
+                                              --sensor_type s1s2 --eval
+        done
       done
     done
   done
 done
-#
